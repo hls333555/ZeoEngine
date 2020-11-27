a85c30dc9e52587832e50551543949d1658b18f4
sansan <1036692596@qq.com>
2020-11-27 14:57:36 +0800

    refactor: premake5.lua

---
+++ Sandbox/premake5.lua
@@ -0,0 +1,58 @@
+project "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir("%{wks.location}/bin-Intermediate/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+	}
+
+	defines
+	{
+		"ZE_IMVEC_GLMVEC_CONV"
+	}
+
+	includedirs
+	{
+		"%{wks.location}/ZeoEngine/vendor/spdlog/include",
+		"%{wks.location}/ZeoEngine/src",
+		"%{wks.location}/ZeoEngine/vendor",
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.glm}",
+		"%{IncludeDir.rttr}",
+		"%{IncludeDir.entt}"
+	}
+
+	links
+	{
+		"ZeoEngine"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines
+		{
+			"ZE_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "ZE_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "ZE_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "ZE_DIST"
+		runtime "Release"
+		optimize "on"
---
+++ ZeoEngine/premake5.lua
@@ -0,0 +1,82 @@
+project "ZeoEngine"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir("%{wks.location}/bin-Intermediate/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "ZEpch.h"
+	pchsource "src/ZEpch.cpp"
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+		"vendor/stb_image/**.h",
+		"vendor/stb_image/**.cpp",
+		"vendor/glm/glm/**.hpp",
+		"vendor/glm/glm/**.ini",
+		"vendor/ImGuizmo/**.h",
+		"vendor/ImGuizmo/**.cpp"
+	}
+
+	defines
+	{
+		"_CRT_SECURE_NO_WARNINGS",
+		"ZE_IMVEC_GLMVEC_CONV"
+	}
+
+	includedirs
+	{
+		"src",
+		"vendor/spdlog/include",
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.Glad}",
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.glm}",
+		"%{IncludeDir.stb_image}",
+		"%{IncludeDir.rttr}",
+		"%{IncludeDir.ImGuizmo}",
+		"%{IncludeDir.rapidjson}",
+		"%{IncludeDir.entt}",
+		"%{IncludeDir.yaml_cpp}",
+		"%{IncludeDir.magic_enum}"
+	}
+
+	links 
+	{ 
+		"GLFW",
+		"Glad",
+		"ImGui",
+		"yaml-cpp",
+		"opengl32.lib",
+		"rttr",
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines
+		{
+			"ZE_PLATFORM_WINDOWS",
+			"ZE_BUILD_DLL",
+			-- If this is defined, glfw3.h will not include gl.h which conflicts with glad.h
+			"GLFW_INCLUDE_NONE"
+		}
+
+	filter "configurations:Debug"
+		defines "ZE_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "ZE_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "ZE_DIST"
+		runtime "Release"
+		optimize "on"
