c661394947c94ab8f8b3cebbbceb8b88d4095878
sansan <1036692596@qq.com>
2020-11-27 21:50:27 +0800

    refactor: avoid using std::function in event dispatcher for performance

--- ZeoEngine/src/Engine/Events/Event.h
+++ ZeoEngine/src/Engine/Events/Event.h
@@ -68,21 +68,19 @@ namespace ZeoEngine {
 
 	class EventDispatcher
 	{
-		template<typename T>
-		using EventFn = std::function<bool(T&)>;
-
 	public:
 		EventDispatcher(Event& event)
 			: m_Event(event)
 		{
 		}
 
-		template<typename T>
-		bool Dispatch(EventFn<T> func)
+		// F will be deduced by the compiler
+		template<typename T, typename F>
+		bool Dispatch(const F& func)
 		{
 			if (m_Event.GetEventType() == T::GetStaticType())
 			{
-				m_Event.m_bHandled = func(*(T*)&m_Event);
+				m_Event.m_bHandled |= func(static_cast<T&>(m_Event));
 				return true;
 			}
 			return false;
